75a38937b6c6c4fa17b54cf026484c9059379c0d
Praveen <552016+spraveenitpro@users.noreply.github.com>
2022-10-26 05:27:59 +0000

    Completed Recursion exercices

--- recursion.js
+++ recursion.js
@@ -1,11 +1,40 @@
-let arr = [2,4,6];
+let arr = [2, 4, 6, 7, 2, 4, 78, 12, 101, 34];
 
-function recursion(arr) {
+// Recursively Add contents of Array
+
+function recursionAdd(arr) {
 	if (arr.length === 0) {
 		return 0;
 	} else {
-		return(arr[0] + recursion(arr.slice(1)));
+		return (arr[0] + recursionAdd(arr.slice(1)));
 	}
 }
 
-console.log(recursion(arr));
+console.log(recursionAdd(arr));
+
+function recursionCount(arr) {
+	if (arr.length === 0) {
+		return 0
+	} else {
+		return (1 + recursionCount(arr.slice(1)));
+	}
+}
+
+console.log(recursionCount(arr));
+
+// recursively find the biggest number in the array in javascript	
+function recursionMax(arr) {
+	if (arr.length === 1) {
+		return arr[0];
+	}
+	if (arr.length === 2) {
+		return arr[0] > arr[1] ? arr[0] : arr[1];
+	}
+	const submax = recursionMax(arr.slice(1));
+	//console.log(submax);
+	return arr[0] > submax ? arr[0] : submax;
+}
+
+console.log(recursionMax(arr));
+
+//console.log(arr.slice(2));
